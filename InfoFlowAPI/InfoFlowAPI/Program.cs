035b3d978a949f7b2fdb5760f819255c2c31094f
PPalac <ppalac@outlook.com>
2018-01-02 18:58:29 +0100

    Added Serilog

--- InfoFlowAPI/InfoFlowAPI/Program.cs
+++ InfoFlowAPI/InfoFlowAPI/Program.cs
@@ -1,25 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace InfoFlowAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .Enrich.FromLogContext()
+          .WriteTo.RollingFile(Path.Combine("Logs", $"Log.txt"))
+          .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                BuildWebHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .Build();
     }
 }
--- InfoFlowAPI/InfoFlowAPI/Startup.cs
+++ InfoFlowAPI/InfoFlowAPI/Startup.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using InfoFlowAPI.Data.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using InfoFlowAPI.Data.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace InfoFlowAPI
 {
